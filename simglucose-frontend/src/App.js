354221fb9171fc354c38fa35e699493bb4773524
Meng Xiao <mxiaopsu@gmail.com>
2021-10-19 09:48:44 -0400

    added requirement doc for backend

--- simglucose-frontend/src/App.js
+++ simglucose-frontend/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import {SimulationPage} from "./Pages/SimulationPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Header from "./Components/header/header";
+import HomePage from "./Pages/homepage/homepage";
+import { SimulationPage } from "./Pages/SimulationPage";
 
 function App() {
   return (
     <div className="App">
-      <SimulationPage/>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/simulate" component={SimulationPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
